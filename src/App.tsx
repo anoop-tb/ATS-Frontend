00f4bec4c402fc54e27cc2c19a88dc625a40fd0a
Sasmita  Rout <sasmita.rout@accionlabs.com>
2025-11-17 23:28:00 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import FilterPage from "./component/pages/filterPage/FilterPage";
 import LoginPage from "./component/pages/loginPage/LoginPage";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import ProtectedRoute from "./component/ProtectedRoute";
 
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<FilterPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
       );
 }
 
