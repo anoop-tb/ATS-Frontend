2faa09cd6f3fc3a403a90021aabbdf0a2e5cb5ab
sandhiyag2021 <sandhiya.g@accionlabs.com>
2025-12-02 17:08:50 +0530

    removed-logout

--- src/component/pages/filterPage/Navbar.tsx
+++ src/component/pages/filterPage/Navbar.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { MenuProps, Popconfirm } from "antd";
-import { Button, Layout, Menu } from "antd";
+import { MenuProps } from "antd";
+import { Layout, Menu } from "antd";
 import { Link } from "react-router-dom";
-import Cookies from "js-cookie";
-import { useNavigate } from "react-router-dom";
 import accion from "../../../accion.png";
 
 import "./style.css";
@@ -11,36 +9,16 @@ import "./style.css";
 const { Header } = Layout;
 
 const Navbar = () => {
-  const navigate = useNavigate();
-
   const menuOption: MenuProps["items"] = [
     {
       key: "home",
-
       label: <Link to="/">Home</Link>,
     },
     {
       key: "prevSearch",
-
       label: <Link to="/previoussearchlist">Previous Search</Link>,
     },
   ];
-
-  const SelectedMenu = (value: string) => {
-    switch (value) {
-      case "Home":
-        return <Link to="/" />;
-      case "Previous Search":
-        return <Link to="/previoussearchlist" />;
-      default:
-        return <Link to="/login" />;
-    }
-  };
-
-  const confirm = () => {
-    Cookies.remove("atsUser");
-    navigate("/login");
-  };
 
   return (
     <Header
@@ -56,18 +34,14 @@ const Navbar = () => {
           margin: "16px 44px 16px 0",
         }}
       />
-      <Popconfirm title="Sure to Logout?" onConfirm={confirm}>
-        <Button type="primary" style={{ float: "right", marginTop: "15px" }}>
-          Logout
-        </Button>
-      </Popconfirm>
+      {/* Uncomment below if you want the menu */}
       {/* <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['home']}
-          style={{ lineHeight: '64px' }}
-          items = {menuOption}
-        /> */}
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['home']}
+        style={{ lineHeight: '64px' }}
+        items={menuOption}
+      /> */}
     </Header>
   );
 };
